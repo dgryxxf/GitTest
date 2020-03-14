8de8bdad96d041b4d6079fc21c4a5e91c07f7f6e
dgryxxf <1205210830@qq.com>
2020-03-14 16:37:52 +0800

    新增了Cat

---
+++ src/com/company/Cat.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @author tinyBee
+ * @date 2020/3/14 16:28
+ * @Description
+ */
+public class Cat extends Animal {
+
+    Cat(String name, int age) {
+        super(name, age);
+    }
+}
